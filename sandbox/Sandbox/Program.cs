f5b8737717bf394d6e4d370f78b486385cca0a35
clybb1 <lyb20004@byui.edu>
2023-02-03 11:24:42 -0700

    sandbox testing

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,48 @@
-using System;
+class Program {
+    static void Main(string[] args) {
+        
+        var divider =  new IntegerDivision();
+        divider.setlhs(10);
+        divider.setrhs(1);
+        divider.DisplayResult();
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Sandbox World!");
+        divider.setlhs(0);
+        divider.setrhs(100);
+        divider.DisplayResult();
+
+        divider.setlhs(5);
+        divider.setrhs(0);
+        divider.DisplayResult();
+
+        divider.setlhs(25);
+        divider.setrhs(5);
+        divider.DisplayResult();
+    }
+}
+
+class IntegerDivision {
+    private int _lhs = 1;
+    private int _rhs = 1;
+
+    public void setrhs(int newrhs){
+        if (newrhs==0){
+            Console.WriteLine("0 is not allowed, defaulting to 0");
+            _rhs = 1;
+        }
+        else{
+            _rhs = newrhs;
+        }
+    }
+    public void setlhs(int newlhs){
+        _lhs = newlhs;
+    }
+
+    public int Result() {
+        return _lhs / _rhs;
+    }
+
+    public void DisplayResult() {
+        var result = Result();
+        Console.WriteLine($"{_lhs} divided by {_rhs} is {result}");
     }
 }
