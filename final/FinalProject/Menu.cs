f45566190248b6f1def2a60e7988fec3da277dd6
Caleb <callyb1233@gmail.com>
2023-03-21 14:51:00 -0600

    base topics creation

--- final/FinalProject/Menu.cs
+++ final/FinalProject/Menu.cs
@@ -1,5 +1,5 @@
 class Menu{
-
+    private List<Topics> _topics = new List<Topics>();
 
     public Menu(){
         var family = new Family();
@@ -10,13 +10,59 @@ class Menu{
         var revelation = new Revelation();
         var temple = new Temple();
         var service = new Service();
+        _topics.Add(family);
+        _topics.Add(agency);
+        _topics.Add(atonement);
+        _topics.Add(addiction);
+        _topics.Add(fasting);
+        _topics.Add(revelation);
+        _topics.Add(temple);
+        _topics.Add(service);
 
     }
 
     public void Display(){
         Console.Clear();
+        bool quit = false;
         while (true){
-            
+            Console.WriteLine("Options:");
+            Console.WriteLine("1. Display random quote");
+            Console.WriteLine("2. Display all quotes");
+            Console.WriteLine("3. quit");
+            Console.Write("Select an option: ");
+            int.TryParse(Console.ReadLine(), out int option);
+            switch(option){
+                case 1:
+                Console.Clear();
+                Console.WriteLine("1. Random topic");
+                Console.WriteLine("1. Choose topic");
+                Console.Write("Please select an option: ");
+                int.TryParse(Console.ReadLine(), out int topic);
+                switch (topic){
+                    case 1:
+                    Random random = new Random();
+                    int randInt = random.Next(_topics.Count());
+                    Topics randomTopic = _topics[randInt];
+                    randomTopic.DisplayRandom();
+                    break;
+                    case 2:
+                    break;
+                    default:
+                    break;
+                }
+                break;
+                case 2:
+                break;
+                case 3:
+                quit = true;
+                break;
+                default:
+                break;
+
+            }
+        if (quit == true){
+            break;
+        }
         }
     }
 }
--- final/FinalProject/Topics.cs
+++ final/FinalProject/Topics.cs
@@ -1,9 +1,8 @@
-class Topics{
+ class Topics{
     protected List<string> _quotes = new List<string>();
 
-    public Topics(){
+    private string _type;
 
-    }
 
     virtual public void DisplayAll(){
 
@@ -26,25 +25,177 @@ class Family:Topics{
         _quotes.Add("this is my quote");
         _quotes.Add("this is my quote");
     }
+
+    override public void DisplayRandom(){
+        Console.WriteLine("From topic Family:");
+        Random random = new Random();
+        int randInt = random.Next(_quotes.Count());
+        string randQuote = _quotes[randInt];
+        Console.WriteLine(randQuote);
+    }
+
+    override public void DisplayAll(){
+
+    }
 }
 class Agency:Topics{
 
+    public Agency(){
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+    }
+
+    override public void DisplayRandom(){
+        Console.WriteLine("From topic Agency:");
+    }
+
+    override public void DisplayAll(){
+
+    }
 }
 class Atonement:Topics{
 
+    public Atonement(){
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+    }
+
+    override public void DisplayRandom(){
+        Console.WriteLine("From topic Atonement:");
+    }
+
+    override public void DisplayAll(){
+
+    }
 }
 class Addiction:Topics{
 
+    public Addiction(){
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+    }
+
+    override public void DisplayRandom(){
+        Console.WriteLine("From topic Addiction");
+    }
+
+    override public void DisplayAll(){
+
+    }
 }
 class Fasting:Topics{
 
+    public Fasting(){
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+    }
+
+    override public void DisplayRandom(){
+        Console.WriteLine("From topic Fasting:");
+    }
+
+    override public void DisplayAll(){
+
+    }
 }
 class Revelation:Topics{
 
+    public Revelation(){
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+    }
+
+    override public void DisplayRandom(){
+        Console.WriteLine("From topic Revelation:");
+    }
+
+    override public void DisplayAll(){
+
+    }
 }
 class Temple:Topics{
 
+    public Temple(){
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+    }
+
+    override public void DisplayRandom(){
+        Console.WriteLine("From topic Temple:");
+    }
+
+    override public void DisplayAll(){
+
+    }
 }
 class Service:Topics{
 
+    public Service(){
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+        _quotes.Add("this is my quote");
+    }
+
+    override public void DisplayRandom(){
+        Console.WriteLine("From topic Service:");
+    }
+
+    override public void DisplayAll(){
+
+    }
 }
